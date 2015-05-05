21d7950d8303b0f900049c351785df61f440130d
Tony Deng <wolf.deng@gmail.com>
2015-05-05 14:58:12 +0800

    测试通过log组件输出

--- src/test/java/com/github/demo/boot/SampleApplicationTest.java
+++ src/test/java/com/github/demo/boot/SampleApplicationTest.java
@@ -2,18 +2,21 @@ package com.github.demo.boot;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.OutputCapture;
 
 /**
  * Created by tonydeng on 15/5/5.
  */
 public class SampleApplicationTest {
+    private static final Logger log = LoggerFactory.getLogger(SampleApplicationTest.class);
     @Rule
     public OutputCapture outputCapture = new OutputCapture();
     @Test
     public void testCommandLineOverrides(){
         SampleApplication.main(new String[]{"--name=Tony"});
         String output=  outputCapture.toString();
-        System.out.println(output);
+        log.info("--------------SampleApplication test commandline output:'{}'", output);
     }
 }
